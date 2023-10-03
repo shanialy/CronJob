800d10c56941b81b2e435bad934a2368236729eb
shanialy <xhanialee1@gmail.com>
2023-10-03 14:10:12 +0500

    fix: update

--- app.js
+++ app.js
@@ -1,78 +1,62 @@
-// const cron = require("node-cron");
-// const { Octokit } = require("@octokit/rest");
-// require("dotenv").config();
-
-// const octokit = new Octokit({
-//   auth: process.env.ACCESS_TOKEN,
-// });
-
-// const commitToGitHub = async () => {
-//   try {
-//     const date = new Date().toISOString();
-//     const fileContent = `Ghost commit on ${date}`;
-//     const content = Buffer.from(fileContent).toString("base64");
-
-//     const response = await octokit.repos.createOrUpdateFileContents({
-//       owner: process.env.REPO_OWNER_NAME,
-//       repo: process.env.REPO_NAME,
-//       path: "ghost-commit.txt" + date,
-//       message: process.env.COMMIT_MESSAGE,
-//       content,
-//       committer: {
-//         name: process.env.REPO_OWNER_NAME,
-//         email: process.env.REPO_OWNER_EMAIL,
-//       },
-//     });
-
-//     console.log(`Commit created: ${response.data.commit.html_url}`);
-//   } catch (error) {
-//     console.error("Error creating commit:", error);
-//   }
-// };
-
-// // cron.schedule("0 0 * * *", () => {
-//   console.log("Running daily commit job...");
-//   commitToGitHub();
-// // });
-
-// console.log("CRON job scheduled.");
-
-// setInterval(() => {}, 1000);
-const git = require('simple-git')();
-const cron = require('node-cron');
+const git = require("simple-git")();
+const cron = require("node-cron");
+const fs = require('fs');
 require("dotenv").config();
+
+function generateRandomTextName(length = 10) {
+  const characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let randomText = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomText += characters.charAt(randomIndex);
+  }
+
+  return randomText;
+}
+
+function deleteExistingTextFiles() {
+  fs.readdirSync(__dirname).forEach((file) => {
+    if (file.endsWith(".txt")) {
+      fs.unlinkSync(file);
+      console.log(`Deleted existing file: ${file}`);
+    }
+  });
+}
 
 const commitToGitHub = async () => {
   try {
     const date = new Date().toISOString();
     const fileContent = `Ghost commit on ${date}`;
-    const filePath = './test.txt'; // Path to the file you want to commit
+    deleteExistingTextFiles();
 
-    // Create or update the file content
-    await git.addConfig('user.name', process.env.REPO_OWNER_NAME);
-    await git.addConfig('user.email', process.env.REPO_OWNER_EMAIL);
-    await git.add(filePath, Buffer.from(fileContent).toString('base64'));
+    const filePath = `./${generateRandomTextName()}.txt`;
 
-    // Commit the changes
+    fs.writeFileSync(filePath, fileContent);
+
+    await git.addConfig("user.name", process.env.REPO_OWNER_NAME);
+
+    await git.addConfig("user.email", process.env.REPO_OWNER_EMAIL);
+    
+    await git.add(filePath, Buffer.from(fileContent).toString("base64"));
+
     await git.commit(process.env.COMMIT_MESSAGE);
 
-    // Push the changes to the remote repository (assuming you have the necessary permissions)
-    await git.push('origin', 'master');
+    await git.push("origin", "master");
 
-    console.log('Commit created and pushed successfully.');
-
+    console.log("Commit created and pushed successfully.");
   } catch (error) {
-    console.error('Error creating and pushing commit:', error);
+    console.error("Error creating and pushing commit:", error);
   }
 };
 
 // Schedule the job to run daily at a specific time (adjust as needed)
 // cron.schedule('0 0 * * *', () => {
-  console.log('Running daily commit job...');
-  commitToGitHub();
+console.log("Running daily commit job...");
+commitToGitHub();
 // });
 
-console.log('CRON job scheduled.');
+console.log("CRON job scheduled.");
 
-// Keep the script running
 setInterval(() => {}, 1000);
